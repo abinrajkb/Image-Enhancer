b4db1c568df9834484d0dd7edb2cec541d9ac89c
abinrajkb <abinrajkb@gmail.com>
2020-04-01 12:06:08 +0530

    Initial commit.

---
+++ articles/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Image(models.Model):
+    name = models.CharField(default='New-image',max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(default='default-slug')
+    raw_image = models.ImageField(default='default.png', blank=True)
+    processed_image = models.ImageField(blank=True)
+    author = models.ForeignKey(User, default=None,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
